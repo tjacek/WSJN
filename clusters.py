34db9e6894fd3fbd6623e8a6749e5ff2202d8489
tjacek <tjacek@student.agh.edu.pl>
2016-05-31 16:09:43 +0200

    cluster lines

--- clusters.py
+++ clusters.py
@@ -1,15 +1,20 @@
 import numpy as np
 #import np.random from  randint
 
-def naive_clusters(lines,metric,eps=10.0):
-    print(len(lines))
-    line_j=lines[0]
-    cluster_j=[]
-    for line_i in lines:
-        dst_ij=metric(line_i,line_j)
-        if(dst_ij<eps):
-            print("OK")
-            cluster_j.append(dst_ij)
-        print(dst_ij)
-
+def cluster_lines(lines,metric,eps=10.0):
+	clusters=[]
+    outside=lines
+    while(len(outside)!=0):
+    	 inside,outside=get_cluster(outside[0],lines,metric,eps)
+         clusters.append(inside)
+    return clusters
     
+def get_cluster(cls_center,lines,metric,eps=10.0):
+    inside=[]
+    outside=[]
+    for line_i in lines:
+    	if(metric(cls_center,line_i)<eps):
+    	    inside.append(line_i)
+        else:
+            outside.append(line_i)
+    return inside,outside
